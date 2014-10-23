dc65c86e5a043658e2c68c21fec58b1ce7733008
VivianiPrimo <viviani.primo@gmail.com>
2014-10-23 11:39:45 -0400

    Hi Jeroen!

--- app/src/main/java/com/primo5/tikit/AdapterActivity.java
+++ app/src/main/java/com/primo5/tikit/AdapterActivity.java
@@ -6,6 +6,7 @@ import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ArrayAdapter;
 
+// adlkfjadfh
 
 public class AdapterActivity extends ArrayAdapter{
     private LayoutInflater inflater;
